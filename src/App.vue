18ec0ba69784236e44469f15e20604aee3ba26fa
Sunil Kumar T <s1728k@gmail.com>
2017-12-22 19:22:37 +0530

    prod1

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,14 @@
 // export default {
 //   name: 'app'
 // }
-import Vue from 'vue'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default-dark.css' // This line here
-import 'vue-material/dist/theme/engine.scss'
+// import Vue from 'vue'
+// import VueMaterial from 'vue-material'
+import 'materialize-css/dist/js/materialize.min.js'
+import 'materialize-css/dist/css/materialize.min.css'
+// import 'vue-material/dist/vue-material.min.css'
+// import 'vue-material/dist/theme/default-dark.css' // This line here
 
-Vue.use(VueMaterial)
+// Vue.use(VueMaterial)
 export default {
   computed: {
     username () {
--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -1,30 +1,18 @@
 <template>
-  <div class="elevation-demo">
-    <md-content class="md-elevation-1">1</md-content>
-    <md-content class="md-elevation-2">2</md-content>
-    <md-content class="md-elevation-3">3</md-content>
-    <md-content class="md-elevation-4">4</md-content>
-    <md-content class="md-elevation-5">5</md-content>
-    <md-content class="md-elevation-6">6</md-content>
-    <md-content class="md-elevation-7">7</md-content>
-    <md-content class="md-elevation-8">8</md-content>
-    <md-content class="md-elevation-9">9</md-content>
-    <md-content class="md-elevation-10">10</md-content>
-    <md-content class="md-elevation-11">11</md-content>
-    <md-content class="md-elevation-12">12</md-content>
-    <md-content class="md-elevation-13">13</md-content>
-    <md-content class="md-elevation-14">14</md-content>
-    <md-content class="md-elevation-15">15</md-content>
-    <md-content class="md-elevation-16">16</md-content>
-    <md-content class="md-elevation-17">17</md-content>
-    <md-content class="md-elevation-18">18</md-content>
-    <md-content class="md-elevation-19">19</md-content>
-    <md-content class="md-elevation-20">20</md-content>
-    <md-content class="md-elevation-21">21</md-content>
-    <md-content class="md-elevation-22">22</md-content>
-    <md-content class="md-elevation-23">23</md-content>
-    <md-content class="md-elevation-24">24</md-content>
-  </div>
+    <div class="row">
+      <div class="col s1">1</div>
+      <div class="col s1">2</div>
+      <div class="col s1">3</div>
+      <div class="col s1">4</div>
+      <div class="col s1">5</div>
+      <div class="col s1">6</div>
+      <div class="col s1">7</div>
+      <div class="col s1">8</div>
+      <div class="col s1">9</div>
+      <div class="col s1">10</div>
+      <div class="col s1">11</div>
+      <div class="col s1">12</div>
+    </div>
 </template>
 
 <script>
@@ -36,22 +24,22 @@
 
 <style lang="scss" scoped>
 
-  @import '~vue-material/dist/theme/engine';
+  // @import '~vue-material/dist/theme/engine';
 
-  .elevation-demo {
-    padding: 16px;
-    display: flex;
-    flex-wrap: wrap;
-  }
+  // .elevation-demo {
+  //   padding: 16px;
+  //   display: flex;
+  //   flex-wrap: wrap;
+  // }
 
-  .md-content {
-    width: 100px;
-    height: 100px;
-    margin: 24px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+  // .md-content {
+  //   width: 100px;
+  //   height: 100px;
+  //   margin: 24px;
+  //   display: flex;
+  //   align-items: center;
+  //   justify-content: center;
+  // }
 </style>
 <!-- 
 <template>
